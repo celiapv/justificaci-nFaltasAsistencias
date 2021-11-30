e1e77ddaf2c894aed9230c50099f3417e46b0f86
reda-daw <84437580+reda-daw@users.noreply.github.com>
2021-11-30 20:35:45 +0100

    2 commit

---
+++ crearhtml.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <title>Justificacion de Faltas</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="contenedor">
+        <div class="cabecera">
+        <div class="logo">
+                <img class="logo" SRC="logo.jpeg" WIDTH=195 HEIGHT=55/>
+            </div>
+            <div class="justificacion">
+                JUSTIFICACION DE FALTA DE ASISTENCIA
+            </div>
+        </div>
+           
+    </div>
+</body>
+</html>
