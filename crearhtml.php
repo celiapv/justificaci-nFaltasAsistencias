f347f0e3f5c1ba2c5fcdb1416f9f1b06340b4679
reda-daw <84437580+reda-daw@users.noreply.github.com>
2021-12-01 12:44:00 +0100

    3 commit

--- crearhtml.php
+++ crearhtml.php
@@ -10,14 +10,46 @@
 <body>
     <div class="contenedor">
         <div class="cabecera">
-        <div class="logo">
-                <img class="logo" SRC="logo.jpeg" WIDTH=195 HEIGHT=55/>
+            <div class="logo">
+                <img class="logo" SRC="logo.jpeg" WIDTH=195 HEIGHT=55 />
             </div>
             <div class="justificacion">
                 JUSTIFICACION DE FALTA DE ASISTENCIA
             </div>
         </div>
-           
+        <table>
+            <tbody>
+                <tr>
+                    <td style="white-space:pre, width:20%, height:100%">
+                           <p><h1>
+                                P
+                            </h1></p>
+                            <p><h1>
+                                O
+                            </h1></p> 
+                            <p><h1>
+                                L
+                            </h1></p>
+                            <p><h1>
+                                L
+                            </h1></p>
+                            <p><h1>
+                                O
+                            </h1></p>
+                           
+                    </td>
+                    <td>
+                        <font face="GillSans Light, Segoe UI Semilight, sans-serif">
+                            <font style="font-size: 10pt;text-align: left;">
+                                <p>D./Dña_________________________________________________________________________</p>
+                                <p>con DNI _______________ y NRP _________________________________________________</p>
+                                <p>Profesor/a de:________________ en el IES Augustobriga de Navalmoral de la Mata. </p>
+                            </font>
+                        </font>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </body>
 </html>
