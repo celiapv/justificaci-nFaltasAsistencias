2ccd15b6eb56d511028c2d87c3d147080bc6c8fc
reda-daw <84437580+reda-daw@users.noreply.github.com>
2021-12-02 00:46:32 +0100

    4 commit

--- crearhtml.php
+++ crearhtml.php
@@ -1,55 +1,116 @@
 <!DOCTYPE html>
-<html>
-
+<html lang="en">
 <head>
-    <meta charset="UTF-8" />
-    <title>Justificacion de Faltas</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <title>Justificacion</title>
 </head>
-
 <body>
-    <div class="contenedor">
-        <div class="cabecera">
+    <table>
+        <tr id="contenedor">
+            <div class="cabecera">
             <div class="logo">
                 <img class="logo" SRC="logo.jpeg" WIDTH=195 HEIGHT=55 />
             </div>
             <div class="justificacion">
-                JUSTIFICACION DE FALTA DE ASISTENCIA
+                JUSTIFICACIÒN DE FALTA DE ASISTENCIA
             </div>
-        </div>
-        <table>
-            <tbody>
-                <tr>
-                    <td style="white-space:pre, width:20%, height:100%">
-                           <p><h1>
-                                P
-                            </h1></p>
-                            <p><h1>
-                                O
-                            </h1></p> 
-                            <p><h1>
-                                L
-                            </h1></p>
-                            <p><h1>
-                                L
-                            </h1></p>
-                            <p><h1>
-                                O
-                            </h1></p>
-                           
-                    </td>
-                    <td>
+    <td class="vertical">
+<h1>
+    A
+    I
+    C
+    N
+    E
+    T
+    S
+    I
+    S
+    A
+</h1>
+<h1>
+    E
+    D
+</h1>
+<h1>
+    A
+    T
+    L
+    A
+    F
+</h1>
+<h1>
+    E
+    D
+</h1>
+<h1>
+    N
+    Ó
+    I
+    C
+    A
+    C
+    I
+    F
+    I
+    T
+    S
+    U
+    J
+</h1>
+    </td>
+            <td>
+                <table>
+                    <tr>
+                        <td>
                         <font face="GillSans Light, Segoe UI Semilight, sans-serif">
-                            <font style="font-size: 10pt;text-align: left;">
-                                <p>D./Dña_________________________________________________________________________</p>
-                                <p>con DNI _______________ y NRP _________________________________________________</p>
-                                <p>Profesor/a de:________________ en el IES Augustobriga de Navalmoral de la Mata. </p>
+                            <font style="font-size: 18pt;text-align: left;">
+                                <p>D./Dña____________________________________________________________________________________________</p>
+                                <p>con DNI _______________ y NRP ____________________________________________________________________</p>
+                                <p>Profesor/a de:___________________________________ en el IES Augustobriga de Navalmoral de la Mata.</p>
+                                
                             </font>
                         </font>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                        <font face="GillSans Light, Segoe UI Semilight, sans-serif">
+                            <font style="font-size: 18pt;text-align: left;">
+                                <h2>DECLARA QUE:</h2>
+                                <p>Faltó a clase en la/s fecha/s_____________________________________________________________________</p>
+                                <p>Horas lectivos:___________________________________________________________________________________</p>
+                                <p>Horas complementarias:____________________________________________________________________________</p>
+                            </font>
+                        </font>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                        <font face="GillSans Light, Segoe UI Semilight, sans-serif">
+                            <font style="font-size: 18pt;text-align: left;">
+                                <p>Por los motivos que se detallan a continuación:____________________________________________________</p>
+                                <p>___________________________________________________________________________________________________</p>
+                                <p>___________________________________________________________________________________________________</p>
+                            </font>
+                        </font>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                        <font face="GillSans Light, Segoe UI Semilight, sans-serif">
+                            <font style="font-size: 18pt;text-align: left;">
+                                <p>Y para que conste y surte a los efectos oportunos, firma la presente en</p>
+                                <p>Navalmoral de la Mata a _________________ de _______________ de 202____</p>
+                            </font>
+                        </font>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
 </body>
 </html>
